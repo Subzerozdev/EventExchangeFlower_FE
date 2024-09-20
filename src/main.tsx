2ff8bb3bfdc2bfa376b57a3e8ea99dd2eaed1051
LeMinhNghiaSE181956 <nghialmse181956@fpt.edu.vn>
2024-09-20 20:20:31 +0700

    delete Scr

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <GoogleOAuthProvider clientId="1082844276097-aio45nb82o2rji8p33khg00umi6kcs9m.apps.googleusercontent.com">
-    <App />
-  </GoogleOAuthProvider>
-);
