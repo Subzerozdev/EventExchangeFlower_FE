ae5134c214e5aebd75ea484a5ea15646661f74ca
LeMinhNghiaSE181956 <nghialmse181956@fpt.edu.vn>
2024-11-19 20:27:25 +0700

    fix

--- src/pages/adminPage/adminReport/AdminReports.tsx
+++ src/pages/adminPage/adminReport/AdminReports.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button, Table, message } from 'antd';
 
 import api from '../../../config/api';
